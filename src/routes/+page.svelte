0fb9ba4be698eb7ff479223c07145a7ae81705b6
Sylvain Corsini <sylvain.corsini@protonmail.com>
2023-08-10 01:13:03 +0200

    feat: add Input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,7 @@
 	import Checkbox from '$components/primitives/checkbox/Checkbox.svelte';
 	import Badge from '$components/primitives/badge/Badge.svelte';
 	import AspectRatio from '$components/primitives/aspect-ratio/AspectRatio.svelte';
+	import Input from '$components/primitives/input/Input.svelte';
 
 	let labelText = 'Hello world';
 </script>
@@ -51,10 +52,14 @@
 <Badge>badge</Badge>
 <Badge href="/">badge with link</Badge>
 
-<AspectRatio ratio={16 / 9} class="bg-muted">
-	<img
-		src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
-		alt="Gray by Drew Beamer"
-		class="rounded-md object-cover h-full w-full"
-	/>
-</AspectRatio>
+<div class="max-w-2xl">
+	<AspectRatio ratio={16 / 9} class="bg-muted">
+		<img
+			src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80"
+			alt="Gray by Drew Beamer"
+			class="rounded-md object-cover h-full w-full"
+		/>
+	</AspectRatio>
+</div>
+
+<Input placeholder="Placeholder" />
